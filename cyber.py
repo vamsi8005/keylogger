0ac7f9040295aece66dfd87c25e3e909d724bd6a
vamsi8005 <kandukuruvamsi2@gmail.com>
2025-05-21 14:08:44 +0530

    Update and rename cyber.py.py to cyber.py

---
+++ cyber.py
@@ -0,0 +1,12 @@
+#Listening
+import socket
+ip = input("enter a ip address:")
+port = int(input("enter a port address:"))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((ip, port))
+server.listen()
+print("waiting for keystrokes......")
+client_socket, client_addr = server.accept()
+while True:
+    data = client_socket.recv(4024).decode()
+    print(data)
